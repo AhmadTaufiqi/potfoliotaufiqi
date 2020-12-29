b3d251d1d88ebdfd9f5cab8d0ab5b6417303faf1
AhmadTaufiqi <ahmadtaufiky@gmail.com>
2020-12-29 07:49:09 +0700

    update new

--- js/main.js
+++ js/main.js
@@ -25,3 +25,34 @@ function linkAction() {
     navMenu.classList.remove('show')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+// =====scroll reveal=====
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
+
+// scroll home 
+sr.reveal('.home-title', {})
+sr.reveal('.button', { delay: 100 })
+sr.reveal('.home-img', { delay: 200 })
+sr.reveal('.home-social-icon', { interval: 100 })
+
+// scroll skills
+sr.reveal('.about-img', {})
+sr.reveal('.about-subtitle', { delay: 100 })
+sr.reveal('.about-text', { delay: 200 })
+
+// scroll skills
+sr.reveal('.skills-subtitle', {})
+sr.reveal('.skills-text', { delay: 100 })
+sr.reveal('.skills-data', { interval: 100 })
+sr.reveal('.skills-img', { delay: 200 })
+
+// scroll work 
+sr.reveal('.work-img', { interval: 200 })
+
+// scroll contact 
+sr.reveal('.contact-input', { interval: 300 })
